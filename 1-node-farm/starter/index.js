bb499bf6dcb456eeec76e3d20d450e72552633e4
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-05-09 14:03:35 +0500

    Asyncronys method for writing and reading files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
-const file =fs.readFileSync("./txt/input.txt","utf-8");
-const text = `The content of the file is: ${file}\nIt was created on ${Date.now()}`
-fs.writeFileSync("./txt/output.txt",text);
-console.log("File Created.");
+
+// Synchronos way for writing/reading files
+// const file =fs.readFileSync("./txt/input.txt","utf-8");
+// const text = `The content of the file is: ${file}\nIt was created on ${Date.now()}`
+// fs.writeFileSync("./txt/output.txt",text);
+// console.log("File Created.");
+
+// Asynchronus way for writing/reading files
+fs.readFile("./txt/start.txt","utf-8",(err,data1)=>{
+    fs.readFile(`./txt/${data1}.txt`,"utf-8",(err,data2)=>{
+        console.log(data2)
+        fs.readFile("./txt/append.txt","utf-8",(err,data3)=>{
+            console.log(data3)
+
+            fs.writeFile("./txt/final.txt",`${data2}\n${data3}`,"utf-8",err=>{
+                console.log("Your file has been written.")
+            })
+        })
+    })
+})
+console.log("Reading and writing files...");
